bf6a1dc8040779619ee5b02bc0d58ca3c554ca27
zaidan16-droid <zaidanshafi6@gmail.com>
2025-06-07 19:27:55 +0700

    App.py

--- App.py
+++ App.py
@@ -1,36 +1,6 @@
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
-import av
-import cv2
-import torch
-import numpy as np
+from ultralytics import YOLO
 
-# Load model YOLOv5 dari PyTorch Hub (bebas dari ukuran file lokal)
 @st.cache_resource
 def load_model():
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+    model = YOLO("yolov8n.pt")  # model kecil
     return model
-
-model = load_model()
-
-# Kelas untuk memproses video frame demi frame
-class ObjectDetection(VideoTransformerBase):
-    def transform(self, frame):
-        image = frame.to_ndarray(format="bgr24")
-
-        # Deteksi objek dengan YOLO
-        results = model(image)
-        result_image = np.squeeze(results.render())  # gambar output dengan bounding box
-
-        return av.VideoFrame.from_ndarray(result_image, format="bgr24")
-
-# Judul aplikasi
-st.title("🧠 Real-Time Object Detection with YOLOv5 + Streamlit WebRTC")
-
-# WebRTC Streamer
-webrtc_streamer(
-    key="object-detection",
-    video_processor_factory=ObjectDetection,
-    media_stream_constraints={"video": True, "audio": False},
-    async_processing=True,
-)
