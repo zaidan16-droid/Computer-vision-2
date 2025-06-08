0524be355b625b5e8c707303c5ed7f302f8063d0
zaidan16-droid <zaidanshafi6@gmail.com>
2025-06-08 14:56:51 +0700

    Delete Utils directory

--- Utils/Detection.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import torch
-
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-
-def detect_objects(image):
-    results = model(image)
-    return results.render()[0]  # gambar dengan bounding box
